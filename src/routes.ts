ab3a57ce7da9f3fd02f9e989d27e63eecac2ce8d
jadergermano <jader.germano@icloud.com>
2020-06-20 11:39:55 -0300

    Initial configuration for NanowwNotes backend

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import {
+    celebrate,
+    Joi
+} from 'celebrate';
+
+const routes = express.Router();
+
+
+//Todo: Create Controllers
+//Todo: Routes call
+
+export default routes
+
+
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { errors } from 'celebrate';
+import cors from 'cors';
+import routes from './routes'
+const app = express();
+app.use(cors({
+    // TODO: define cors origin when domain gets done
+    /*  origin: 'www'*/
+}));
+app.use(express.json());
+app.use(errors());
+app.use(routes);
+
+app.listen(3335);
